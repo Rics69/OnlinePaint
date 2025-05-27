81e9ed9702189c109379a2e5d0a4ec8238d8f361
Rics <danil.klementev.29@gmail.com>
2025-05-27 21:20:17 +0300

    feat: add websocket on client

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,25 +1,63 @@
 import '../styles/canvas.scss';
 import {observer} from "mobx-react-lite";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import canvasState from "../store/canvasState.ts";
 import toolState from "../store/toolState.ts";
 import Brush from "../tools/Brush.ts";
+import {Button, Modal} from "react-bootstrap";
+import {useParams} from "react-router-dom";
 
 const Canvas = observer(() => {
     const canvasRef = useRef()
+    const usernameRef = useRef()
+    const [modal, setModal] = useState(true)
+    const params = useParams()
 
     useEffect(() => {
         canvasState.setCanvas(canvasRef.current);
         toolState.setTool(new Brush(canvasRef.current))
     }, [])
 
+    useEffect(() => {
+        if (canvasState.username) {
+            const socket = new WebSocket('ws://localhost:8080');
+            socket.onopen = () => {
+                console.log("Подключение установлено");
+                socket.send(JSON.stringify({
+                    id: params.id,
+                    username: canvasState.username,
+                    method: "connection"
+                }))
+            }
+        }
+    }, [canvasState.username])
+
     const mouseDownHandler = () => {
         canvasState.pushToUndo(canvasRef.current.toDataURL());
     }
 
+    const connectHandler = () => {
+        canvasState.setUsername(usernameRef.current.value);
+        setModal(false);
+    }
+
     return (
         <div className="canvas">
-            <canvas onMouseDown={() => mouseDownHandler()} ref={canvasRef} width={600} height={400} />
+            <Modal show={modal} onHide={() => {
+            }}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Введите ваше имя</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input type="text" ref={usernameRef}/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => connectHandler()}>
+                        Войти
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <canvas onMouseDown={() => mouseDownHandler()} ref={canvasRef} width={600} height={400}/>
         </div>
     );
 });
