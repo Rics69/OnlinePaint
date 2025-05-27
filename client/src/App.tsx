f1d229a4bbd44a44654fd764a4dc8df3cb5b4312
Rics <danil.klementev.29@gmail.com>
2025-05-27 19:45:04 +0300

    feat: add routing and bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,28 @@ import './styles/app.scss';
 import ToolBar from "./components/ToolBar.tsx";
 import SettingBar from "./components/SettingBar.tsx";
 import Canvas from "./components/Canvas.tsx";
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+function DrawingPage() {
+    return (
+        <>
+            <ToolBar/>
+            <SettingBar/>
+            <Canvas/>
+        </>
+    );
+}
 
 function App() {
     return (
-        <div className="app">
-            <ToolBar />
-            <SettingBar />
-            <Canvas />
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <Routes>
+                    <Route path="/:id" element={<DrawingPage />} />
+                    <Route path="*" element={<Navigate to={`/${(+new Date()).toString(16)}`} replace />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
 
